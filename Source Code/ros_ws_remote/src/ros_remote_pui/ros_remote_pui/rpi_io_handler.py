b5c66cc627e24fead57e571b9eefb615bf1778d3
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2024-11-07 06:47:48 +0300

    Raspberry Pi IO handler work started.

--- Source Code/ros_ws_remote/src/ros_remote_pui/ros_remote_pui/rpi_io_handler.py
+++ Source Code/ros_ws_remote/src/ros_remote_pui/ros_remote_pui/rpi_io_handler.py
@@ -15,3 +15,10 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https: www.gnu.org/licenses/>.
+
+import RPi.GPIO as GPIO
+
+
+class RpiIoHandler:
+    def __init__(self):
+        pass
